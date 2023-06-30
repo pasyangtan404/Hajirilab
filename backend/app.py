6866fa620fea48a67c72f87d8c6777edb6472219
pasyangtan404 <pasyangtan101@gmail.com>
2023-06-30 20:25:36 +0545

    added

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/hajirilabdb'
 
@@ -23,7 +25,6 @@ class Users(db.Model):
 
 # with app.app_context():
 #     db.create_all()
-
 
 class UserSchema(ma.Schema):
     class Meta:
@@ -50,84 +51,152 @@ def login():
         return jsonify({'success': True})
     else:
         return jsonify({'success': False, 'message': 'Invalid username or password'})
+    
+    
+class Emp_details(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    employee_id = db.Column(db.String(20), unique=True, nullable=False)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(20), nullable=False)
+    gender = db.Column(db.String(10), nullable=False)
+    dob = db.Column(db.Date, nullable=False)
+    email = db.Column(db.String(50), unique=True)
+    phone_num = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    department = db.Column(db.String(50), nullable=False)
+    position = db.Column(db.String(50), nullable=False)
+    photo_sample = db.Column(db.String(10), nullable=False)
+    
+    def __init__(self, employee_id, first_name, last_name, gender, dob, email, phone_num, address, department, position, photo_sample):
+        self.employee_id = employee_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.dob = dob
+        self.email = email
+        self.phone_num = phone_num
+        self.address = address
+        self.department = department
+        self.position = position
+        self.photo_sample = photo_sample
+        
+# class DetailSchema(ma.Schema):
+#     class Meta:
+#         fields = ('id', 'username', 'password', 'email')
 
 
-# @app.route('/get', methods=['GET'])
-# def home():
-#     all_home = Users.query.all()
-#     results = users_schema.dump(all_home)
-#     return jsonify(results)
+# user_schema = UserSchema()
+# users_schema = UserSchema(many=True)
+
+@app.route('/save', methods=['POST'])
+def save_emp_details():
+    try:
+        print("Save button clicked!")
+        data = request.get_json()
+        
+        employee_id = data.get('employee_id')
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        gender = data.get('gender')
+        dob = data.get('dob')
+        email = data.get('email')
+        phone_num = data.get('phone_num')
+        address = data.get('address')
+        department = data.get('department')
+        position = data.get('position')
+        photo_sample = data.get('photo_sample')
+        
+        new_emp = Emp_details(employee_id, first_name, last_name, gender, dob, email, phone_num, address, department, position, photo_sample)
+        
+        db.session.add(new_emp)
+        
+        db.session.commit()
+        
+        return jsonify({'message': 'Employee details saved successfully'})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@app.route('/update', methods=['POST'])
+def update_emp_details():
+    try:
+        employee_id = request.form.get('employee_id')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        gender = request.form.get('gender')
+        dob = request.form.get('dob')
+        email = request.form.get('email')
+        phone_num = request.form.get('phone_num')
+        address = request.form.get('address')
+        department = request.form.get('department')
+        position = request.form.get('position')
+        photo_sample = request.form.get('photo_sample')
+        
+         # Retrieve the existing employee details from the database based on employee_id
+        emp = Emp_details.query.filter_by(employee_id=employee_id, first_name = first_name, last_name = last_name, gender = gender, dob = dob, email = email, phone_num = phone_num, address = address, department = department, position = position, photo_sample = photo_sample).first()
+        
+        if emp:
+            employee_id = request.form.get('employee_id')
+            first_name = request.form.get('first_name')
+            last_name = request.form.get('last_name')
+            gender = request.form.get('gender')
+            dob = request.form.get('dob')
+            email = request.form.get('email')
+            phone_num = request.form.get('phone_num')
+            address = request.form.get('address')
+            department = request.form.get('department')
+            position = request.form.get('position')
+            photo_sample = request.form.get('photo_sample')
+            
+            db.session.commit()
+            
+            return jsonify({'message': 'Employee details updated successfully'})
+        else:
+            return jsonify({'error': 'Employee not found'}), 404
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/delete', methods=['POST'])
+def delete_emp_details():
+    try:
+        employee_id = request.form.get('employee_id')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        gender = request.form.get('gender')
+        dob = request.form.get('dob')
+        email = request.form.get('email')
+        phone_num = request.form.get('phone_num')
+        address = request.form.get('address')
+        department = request.form.get('department')
+        position = request.form.get('position')
+        photo_sample = request.form.get('photo_sample')   
+        
+        emp = Emp_details.query.filter_by(employee_id=employee_id, first_name = first_name, last_name = last_name, gender = gender, dob = dob, email = email, phone_num = phone_num, address = address, department = department, position = position, photo_sample = photo_sample).first()
+        
+        if emp:
+            db.session.delete(emp)
+            
+            db.session.commit()
+
+            return jsonify({'message': 'Employee details deleted successfully'})
+        else:
+            return jsonify({'error': 'Employee not found'}), 404
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
-# @app.route('/get/<id>/', methods=['GET'])
-# def post_details(id):
-#     article = Users.query.get(id)
-#     return user_schema.jsonify(article)
 
-# @app.route('/add', methods=['POST'])
-# def add_home():
-#     username = request.json['username']
-#     password = request.json['password']
-#     email = request.json['email']
 
-#     users = Users(username, password, email)
-#     db.session.add(users)
-#     db.session.commit()
-#     return user_schema.jsonify(users)
 
-# @app.route('/update/<id>/', methods=['PUT'])
-# def update_home(id):
-#     article = Users.query.get(id)
 
-#     username = request.json['username']
-#     password = request.json['password']
-#     email = request.json['email']
 
-#     article.username = username
-#     article.password = password
-#     article.email = email
-#     db.session.commit()
-#     return user_schema.jsonify(article)
 
-# @app.route('/delete/<id>/', methods=['DELETE'])
-# def delete_home(id):
-#     article = Users.query.get(id)
-#     db.session.delete(article)
-#     db.session.commit()
-#     return user_schema.jsonify(article)
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# from flask import Flask,render_template,Response
-# import cv2
-
-# app=Flask(__name__)
-# camera=cv2.VideoCapture(0)
-
-# def generate_frames():
-#     while True:
-            
-#         ## read the camera frame
-#         success,frame=camera.read()
-#         if not success:
-#             break
-#         else:
-#             ret,buffer=cv2.imencode('.jpg',frame)
-#             frame=buffer.tobytes()
-
-#         yield(b'--frame\r\n'
-#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-# @app.route('/')
-# def index():
-#     return render_template('./node')
-
-# @app.route('/video')
-# def video():
-#     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
-
-# if __name__=="__main__":
-#     app.run(debug=True)
--- src/ui/js/menu.js
+++ src/ui/js/menu.js
@@ -1,5 +1,6 @@
 const { ipcRenderer } = require('electron');
 
+/*----------- for minimize, maximize, and close button ------------*/
 // minimize button
 document.getElementById('menu-minm-btn').addEventListener('click', () => {
     ipcRenderer.send('minimize-window');
@@ -15,6 +16,8 @@ document.getElementById('menu-close-btn').addEventListener('click', () => {
     ipcRenderer.send('close-window');
 })
 
+
+/*--------- for sidebar and content change ------------*/
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
 
@@ -53,8 +56,108 @@ options.forEach(function (option) {
     });
 });
 
-// const logoutLink = document.querySelector('#log-out');
+/*----------- for save, update and delete button ---------------*/
+// document.getElementById('save-btn').addEventListener('click', () => {
+//     event.preventDefault();
+//     var formData = new FormData(document.querySelector('form'));
 
-// logoutLink.addEventListener('click', function () {
-//     ipcRenderer.send('logout-btn');
+//     fetch('/save', {
+//         method: 'POST',
+//         body: formData
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// });
+
+
+document.getElementById('save-btn').addEventListener('click', () => {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const employee_id = document.querySelector('#employee-id').value;
+        const first_name = document.querySelector('#first-name').value;
+        const last_name = document.querySelector('#last-name').value;
+        const gender = document.querySelector('#gender').value;
+        const dob = document.querySelector('#dob').value;
+        const email = document.querySelector('#email').value;
+        const phone_num = document.querySelector('#phone').value;
+        const address = document.querySelector('#address').value;
+        const department = document.querySelector('#department').value;
+        const position = document.querySelector('#position').value;
+        const photo_sample = document.querySelector('input[name="optionsRadios"]:checked').value;
+
+        console.log(first_name)
+        console.log(last_name)
+
+        const data = {
+            employee_id: employee_id,
+            first_name: first_name,
+            last_name: last_name,
+            gender: gender,
+            dob: dob,
+            email: email,
+            phone_num: phone_num,
+            address: address,
+            department: department,
+            position: position,
+            photo_sample: photo_sample
+        }
+
+        fetch('http://127.0.0.1:5000/save', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            alert('Employee details saved successfully');
+        })
+        .catch(error => {
+            console.error(error);
+            alert('An error occurred while saving employee details');
+        });
+    });
+});
+
+// document.getElementById('update-btn').addEventListener('click', () => {
+//     event.preventDefault();
+//     var formData = new FormData(document.querySelector('form'));
+
+//     fetch('/update', {
+//         method: 'POST',
+//         body: formData
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// });
+
+// document.getElementById('delete-btn').addEventListener('click', () => {
+//     event.preventDefault();
+//     fetch('/delete', {
+//         method: 'POST'
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
 // });
